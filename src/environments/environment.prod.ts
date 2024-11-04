db9558cd82de046c010fcdff67452b971cca05ca
samsom <samsom.dewitsegid@dektech.com>
2024-11-04 18:12:54 +0100

    Add dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
+    
     // Replace with your production API URL
     //'https://your-api-service.onrender.com/api'
     apiUrl: 'https://bookquoteapp.onrender.com/'  
